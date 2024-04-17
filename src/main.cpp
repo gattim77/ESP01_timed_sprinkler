ceafb762681c496ffad573250da97cb774f6369f
Marco Gatti <gattim@gmail.com>
2024-04-17 19:45:16 +0200

    timer funzionante

--- src/main.cpp
+++ src/main.cpp
@@ -7,25 +7,31 @@
 void setup() {
   //Serial.begin(9600);
   pinMode(GPIO_PIN, OUTPUT);
+
+
+  // Activate GPIO2
+  //Serial.println("Activate pump");
+  digitalWrite(GPIO_PIN, HIGH);
+  delay(3000); // Wait for 3 seconds
+  // Deactivate GPIO2
+  //Serial.println("Deactivate pump");
+  digitalWrite(GPIO_PIN, LOW);
+  //Serial.println("Going to sleep");
+  //delay(1000); // Wait for 1 second
+  //delay(600000); // Wait for 10 minutes
+  //Serial.end();
+  ESP.deepSleep(86400e6); //deepsleep 1 day (86400 seconds - 86400 x 10e6 microseconds)
+  delay(10);
+
+
 }
 
 void loop() {
 
-int sensorValue = analogRead(ANALOG_PIN);
+  //int sensorValue = analogRead(ANALOG_PIN);
 
   // Print the sensor value to the serial monitor
   //Serial.print("light: ");
   //Serial.println(sensorValue);
-
-  // Activate GPIO2
-  Serial.println("Activate pump");
-  digitalWrite(GPIO_PIN, HIGH);
-  delay(4000); // Wait for 4 seconds
-  // Deactivate GPIO2
-  Serial.println("Deactivate pump");
-  digitalWrite(GPIO_PIN, LOW);
-  Serial.println("sleep");
-  delay(1000); // Wait for 1 second
-  //delay(600000); // Wait for 10 minutes
   
 }
