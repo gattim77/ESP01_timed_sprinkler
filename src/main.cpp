3c0b525456973417bc3aae2524bf13945cb10ac6
Marco Gatti <gattim@gmail.com>
2024-04-17 16:34:57 +0200

    funziona, no deep sleep yet. moved to ESP12 due to lack of programmer adapter.

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,31 @@
 #include <Arduino.h>
 
-#define GPIO_PIN 2 // GPIO2 is connected to pin 2 on ESP-01
-
+#define GPIO_PIN 13 // GPIO13/D7 is connected to pin 13 on ESP12
+#define ANALOG_PIN A0
 
 
 void setup() {
+  //Serial.begin(9600);
   pinMode(GPIO_PIN, OUTPUT);
-  
 }
 
 void loop() {
-// Activate GPIO2
-  Serial.print("Activate pump");
+
+int sensorValue = analogRead(ANALOG_PIN);
+
+  // Print the sensor value to the serial monitor
+  //Serial.print("light: ");
+  //Serial.println(sensorValue);
+
+  // Activate GPIO2
+  Serial.println("Activate pump");
   digitalWrite(GPIO_PIN, HIGH);
   delay(4000); // Wait for 4 seconds
   // Deactivate GPIO2
-  Serial.print("Deactivate pump");
+  Serial.println("Deactivate pump");
   digitalWrite(GPIO_PIN, LOW);
-  Serial.print("sleep");
-  delay(600000); // Wait for 10 minutes
+  Serial.println("sleep");
+  delay(1000); // Wait for 1 second
+  //delay(600000); // Wait for 10 minutes
   
-  Serial.print("*");
-
 }
