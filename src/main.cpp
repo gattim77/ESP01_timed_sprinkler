ab4f7cfc33e76f120d89accbb4d10f0ddd5222bb
Marco Gatti <gattim@gmail.com>
2024-04-17 22:33:10 +0200

    Deep sleep modificato con store dello stato nell' RTC. tempo da modificare, ora veloce per debug

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,56 @@
 #include <Arduino.h>
+#include <user_interface.h>
 
 #define GPIO_PIN 13 // GPIO13/D7 is connected to pin 13 on ESP12
 #define ANALOG_PIN A0
 
 
+#define RTC_BASE 65
+#define SLEEP_COUNTS_FOR_WATER 24 // water every 24 hours
+
+
+
+uint32_t sleepCount;
+
+
 void setup() {
-  //Serial.begin(9600);
+  Serial.begin(9600);
+  //delay(1000);
   pinMode(GPIO_PIN, OUTPUT);
 
 
-  // Activate GPIO2
-  //Serial.println("Activate pump");
-  digitalWrite(GPIO_PIN, HIGH);
-  delay(3000); // Wait for 3 seconds
-  // Deactivate GPIO2
-  //Serial.println("Deactivate pump");
-  digitalWrite(GPIO_PIN, LOW);
+  system_rtc_mem_read(RTC_BASE, &sleepCount, 4); // read counter
+  sleepCount++;
+  if (sleepCount > SLEEP_COUNTS_FOR_WATER) {
+    //do watering and reset
+    // Activate GPIO2
+    Serial.println("Activate pump");
+    digitalWrite(GPIO_PIN, HIGH);
+    delay(3000); // Wait for 3 seconds
+    // Deactivate GPIO2
+    Serial.println("Deactivate pump");
+    digitalWrite(GPIO_PIN, LOW);
+     
+    //reset the memory
+    sleepCount = 0;
+  }
+  // store the counter
+  system_rtc_mem_write(RTC_BASE, &sleepCount, 4); // write counter
+  
+  Serial.println(sleepCount);
+
+
   //Serial.println("Going to sleep");
   //delay(1000); // Wait for 1 second
   //delay(600000); // Wait for 10 minutes
+  //ESP.deepSleep(86400e6); //deepsleep 1 day (86400 seconds - 86400 x 10e6 microseconds)
+  //this doesnt work as it;s too long. 
+  //Serial.println(ESP.deepSleepMax());
+  yield();
   //Serial.end();
-  ESP.deepSleep(86400e6); //deepsleep 1 day (86400 seconds - 86400 x 10e6 microseconds)
+  Serial.flush();  
+  //ESP.deepSleep (3600e6); 1 hour
+  ESP.deepSleep(2e6); //deepsleep 2 seconds (86400 seconds - 86400 x 10e6 microseconds)
   delay(10);
 
 
