e58ccda49e9cc9b05d1c4b16db40cb2e239eac1c
Marco Gatti <gattim@gmail.com>
2024-04-29 18:46:01 +0200

    working!

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include <Arduino.h>
 #include <user_interface.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
+
+
+
 
 #define GPIO_PIN 4 // GPIO4/D2 
 //apparently only D4 and D5 don't send a short pulse on reset
@@ -8,7 +14,15 @@
 
 #define RTC_BASE 65
 #define SLEEP_COUNTS_FOR_WATER 24 // water every 24 hours
+const unsigned long WIFI_TIMEOUT = 15000; // 15 seconds
 
+const char* SERVER_URL = "http://192.168.178.68/receive_data.php";
+WiFiClient wifiClient; // Create a WiFiClient object
+
+
+//functions defintions
+void connectToWiFi(); 
+void writetoDB(int humidity, int counter);
 
 
 uint32_t sleepCount;
@@ -18,16 +32,22 @@ void setup() {
   Serial.begin(74880);
   //delay(1000);
 
+  connectToWiFi();
 
   system_rtc_mem_read(RTC_BASE, &sleepCount, 4); // read counter
   sleepCount++;
+  if (sleepCount >= SLEEP_COUNTS_FOR_WATER) {
+    writetoDB (1,sleepCount);
+  } else {
+    writetoDB (0,sleepCount);
+  }
   if (sleepCount >= SLEEP_COUNTS_FOR_WATER) {
     pinMode(GPIO_PIN, OUTPUT);
     //do watering and reset
     // Activate GPIO2
     Serial.println("Activate pump");
     digitalWrite(GPIO_PIN, HIGH);
-    delay(2500); // Wait for 2.5 seconds
+    delay(2000); // Wait for 2 seconds
     // Deactivate GPIO2
     Serial.println("Deactivate pump");
     digitalWrite(GPIO_PIN, LOW);
@@ -41,9 +61,6 @@ void setup() {
   Serial.println(sleepCount);
 
 
-  //Serial.println("Going to sleep");
-  //delay(1000); // Wait for 1 second
-  //delay(600000); // Wait for 10 minutes
   //ESP.deepSleep(86400e6); //deepsleep 1 day (86400 seconds - 86400 x 10e6 microseconds)
   //this doesnt work as it;s too long. 
   //Serial.println(ESP.deepSleepMax());
@@ -65,4 +82,55 @@ void loop() {
   //Serial.print("light: ");
   //Serial.println(sensorValue);
   
+}
+
+void connectToWiFi() {
+  Serial.println("Connecting to WiFi...");
+  WiFi.begin(SSID_NAME, SSID_PASSWORD);
+
+  unsigned long startTime = millis();
+  while (WiFi.status() != WL_CONNECTED) {
+    if (millis() - startTime >= WIFI_TIMEOUT) {
+      Serial.println("WiFi connection timed out!");
+      return;
+    }
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void writetoDB(int humidity, int counter){
+
+  // Create JSON payload
+  StaticJsonDocument<200> jsonDocument;
+  jsonDocument["humidity"] = humidity;
+  jsonDocument["action_log"] = counter;
+
+  // Serialize JSON to string
+  String jsonString;
+  serializeJson(jsonDocument, jsonString);
+
+  // Send HTTP POST request to server
+  HTTPClient http;
+  http.begin(wifiClient, SERVER_URL);
+  http.addHeader("Content-Type", "application/json");
+  int httpResponseCode = http.POST(jsonString);
+
+  // Check for successful HTTP POST request
+  if (httpResponseCode > 0) {
+    Serial.print("HTTP POST request successful, response code: ");
+    Serial.println(httpResponseCode);
+  } else {
+    Serial.print("HTTP POST request failed, error code: ");
+    Serial.println(httpResponseCode);
+  }
+
+  // Free resources
+  http.end();
+
+
 }
